fb6be8daa7e9e2c8a830a3954efa01a3d3788b31
woghd9072 <woghd9072@gmail.com>
2019-02-12 17:43:54 +0900

    Dependency setting & Domain mapping & Test

---
+++ day 14/Spring-Boot-Community-Web2/src/main/java/com/jaehong/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.jaehong.repository;
+
+import com.jaehong.domain.Board;
+import com.jaehong.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    Board findByUser(User user);
+}
